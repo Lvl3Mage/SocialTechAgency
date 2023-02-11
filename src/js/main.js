cb71e3b017f4855280d3455a5594786277e3edc7
Lvl3Mage <lvlthreemage@gmail.com>
2023-02-11 18:12:04 +0100

    Completed index.html and footer
    TODO: add bg image to footer

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,39 @@
 var $ = require( "jquery" );
 //IBG  // Also include in SCSS
-// import ibg from './libs/ibg.js'
-// $(document).ready(function(){
-// 	ibg();
-// })
+import ibg from './libs/ibg.js'
+$(document).ready(function(){
+	ibg();
+})
 
 //MODAL  // Also include in SCSS
 // import './libs/modal.js';
 // import {CloseModal, OpenModal} from './libs/modal.js';
 
+$(document).ready(function(){
+	$(".def-slider-3").slick({
+		arrows:true,
+		dots:true,
+		infinite: true,
+		centerMode: true,
+		centerPadding: "0px",
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		responsive: [
+			{
+				breakpoint: 992,
+				settings: {
+					slidesToShow: 1,
+					centerPadding: "20%",
+					slidesToScroll: 1
+				}
+			},{
+				breakpoint: 578,
+				settings: {
+					slidesToShow: 1,
+					centerPadding: "0px",
+					slidesToScroll: 1
+				}
+			}
+		]
+	});
+});
