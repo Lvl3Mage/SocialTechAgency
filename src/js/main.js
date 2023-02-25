dccec861a0a2eb70bb1b2e4660aa33d5ab5dcaa9
Lvl3Mage <lvlthreemage@gmail.com>
2023-02-25 21:00:23 +0100

    Added different headers, footers and pages

--- src/js/main.js
+++ src/js/main.js
@@ -36,4 +36,54 @@ $(document).ready(function(){
 			}
 		]
 	});
+	$(".gallery-slider").slick({
+		rows: 3,
+		arrows:true,
+		dots:true,
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		infinite: true,
+		centerMode: true,
+		centerPadding: "0px",
+		responsive: [
+			{
+				breakpoint: 992,
+				settings: {
+					rows: 2,
+					slidesToShow: 1,
+					centerPadding: "20%",
+					slidesToScroll: 1
+				}
+			},{
+				breakpoint: 578,
+				settings: {
+					rows: 1,
+					slidesToShow: 1,
+					centerPadding: "0px",
+					slidesToScroll: 1
+				}
+			}
+		]
+	});
 });
+
+$(document).on( "click", "[data-mob-menu-trigger]", function(){
+	$("[data-mob-menu-trigger]").toggleClass("active");
+	$(".mob-menu").toggleClass("active");
+});
+$(document).on( "click", "[data-dropdown-menu-trigger]", function(){
+	let container = $(this).closest('[data-dropdown-menu-container]');
+	let dropdowns = container.find('[data-dropdown]');
+	let curDropdown = $(this).closest("[data-dropdown-wrapper]").find("[data-dropdown]");
+	for(let dropdown of dropdowns){
+		if(dropdown != curDropdown[0]){
+			$(dropdown).animate({
+				height: "0px",
+			}, 600);	
+		}
+	}
+	console.log(curDropdown)
+	curDropdown.animate({
+		height: curDropdown.children().css('height'),
+	}, 600);
+})
